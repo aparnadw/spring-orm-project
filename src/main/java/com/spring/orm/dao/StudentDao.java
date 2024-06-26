cb1454cf70664a50176d9bb38a118e41a824f9d3
aparnadw <aparnadw03@gmail.com>
2024-06-26 21:50:33 +0530

    update config

--- src/main/java/com/spring/orm/dao/StudentDao.java
+++ src/main/java/com/spring/orm/dao/StudentDao.java
@@ -1,15 +1,16 @@
 package com.spring.orm.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 
 import com.spring.orm.entites.Student;
+
+import jakarta.transaction.Transactional;
 
 public class StudentDao {
 
 	private HibernateTemplate hibernateTemplate; 
 	
-	@Autowired
+	@Transactional
 	public int insert(Student student) {
 		
 		Integer i = (Integer) this.hibernateTemplate.save(student);
